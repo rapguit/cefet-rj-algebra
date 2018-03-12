196bfb5b044b3c18e27bf9c2eb0afd901aeb64d5
Raphael Fialho <raphael.fialho@b2wdigital.com>
2018-03-12 14:55:18 -0300

    tentativa do metodo de cholesky

--- src/main/java/br/cefet/rj/algebra/service/Cholesky.java
+++ src/main/java/br/cefet/rj/algebra/service/Cholesky.java
@@ -1,12 +1,61 @@
 package br.cefet.rj.algebra.service;
 
 import br.cefet.rj.algebra.model.Result;
+import br.cefet.rj.algebra.util.ArraysUtils;
 
 public class Cholesky implements Method {
 
-    @Override
-    public Result calculate(double[][] input) {
-        return new Result();
-    }
+	@Override
+	public Result calculate(double[][] input) {
+		final Result result = new Result();
+		double in[][] = ArraysUtils.copyWithoutB(input);
+		double l[][] = calculateL(in);
+		double lt[][] = transposedOf(l);
+		double vectorB[] = ArraysUtils.vectorB(input);
+		double vectorY[] = calculateY(l, vectorB);
+		double vectorX[] = calculateX(lt, vectorY);
 
+
+		result.registerMatrix("L", l);
+		result.registerMatrix("LT", lt);
+		result.setSolution(vectorX);
+		return result;
+	}
+
+	private double[][] calculateL(double[][] in) {
+		double l[][] = new double[in.length][in.length];
+		double pivot = 0.0;
+
+		for (int i = 0; i < l.length; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				if (i == j) pivot = in[i][j];
+				if (i == 0 && j == 0) {
+					pivot = Math.sqrt(in[i][j]);
+					l[i][j] = pivot;
+				} else {
+					l[i][j] = in[i][j] / pivot;
+				}
+			}
+		}
+
+		return l;
+	}
+
+	private double[][] transposedOf(double[][] l) {
+		double transposed[][] = new double[l.length][l.length];
+		for (int i = 0; i < l.length; i++) {
+			for (int j = 0; j < l.length; j++) {
+				transposed[j][i] = l[i][j];
+			}
+		}
+		return transposed;
+	}
+
+	private double[] calculateY(double[][] l, double[] vectorB) {
+		return new double[0];
+	}
+
+	private double[] calculateX(double[][] lt, double[] vectorY) {
+		return new double[0];
+	}
 }
---
+++ src/test/java/br/cefet/rj/algebra/service/CholeskyTest.java
@@ -0,0 +1,43 @@
+package br.cefet.rj.algebra.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.cefet.rj.algebra.model.Result;
+
+public class CholeskyTest {
+
+	private Cholesky m;
+
+	@Before
+	public void setUp() throws Exception {
+		m = new Cholesky();
+	}
+
+	@Test
+	public void calculate() {
+		double input[][] = new double[][] {
+				{ 9.0, 3.0, 5.0,  2.0, 20.0 },
+				{ 3.0, 1.0, 8.0,  2.0, 15.0 },
+				{ 2.0, 1.0, 3.0,  6.0,  9.0 },
+				{ 2.0, 5.0, 1.0,  9.0,  3.0 }
+		};
+
+		Result result = m.calculate(input);
+
+//		assertThat(result.getMatrixRegister().get("L"), equalTo(expectedMatrix()));
+	}
+
+	private double[] expectedSolution() {
+		return new double[] { -1.0, 0.0, 1.0, 2.0 };
+	}
+
+	private double[][] expectedMatrix() {
+		return new double[][]{
+				{  2.0, 0.0, 0.0, 0.0 },
+				{  3.0, 8.5, 0.0, 0.0 },
+				{ -2.0, 5.0, 2.4, 0.0 },
+				{  1.0, 2.0, 0.0, 0.3 }
+		};
+	}
+}

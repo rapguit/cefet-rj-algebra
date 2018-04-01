c2c3aac793db4fc522065a7cf7390243e1b5cca2
Raphael Fialho <rapguit@gmail.com>
2018-03-31 23:37:16 -0300

    refatorando metodo cholesky, mas sem validacao

--- src/test/java/br/cefet/rj/algebra/service/CholeskyTest.java
+++ src/test/java/br/cefet/rj/algebra/service/CholeskyTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 
 import br.cefet.rj.algebra.model.Result;
 
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CholeskyTest {
 
 	private Cholesky m;
@@ -15,17 +20,54 @@ public class CholeskyTest {
 	}
 
 	@Test
-	public void calculate() {
+	public void calculate_simple() {
 		double input[][] = new double[][] {
-				{ 9.0, 3.0, 5.0,  2.0, 20.0 },
-				{ 3.0, 1.0, 8.0,  2.0, 15.0 },
-				{ 2.0, 1.0, 3.0,  6.0,  9.0 },
-				{ 2.0, 5.0, 1.0,  9.0,  3.0 }
+				{  4.0,  2.0, -4.0,  0.0 },
+				{  2.0, 10.0,  4.0,  6.0 },
+				{ -4.0,  4.0,  9.0,  5.0 }
 		};
 
 		Result result = m.calculate(input);
 
-//		assertThat(result.getMatrixRegister().get("L"), equalTo(expectedMatrix()));
+		Map<String, Double[][]> matrixRegister = result.getMatrixRegister();
+		assertThat(matrixRegister.get("G"), equalTo(expectedSimpleMatrix()));
+		assertThat(matrixRegister.get("GT"), equalTo(expectedTransposedSimpleMatrix()));
+		assertThat(result.getSolution(), equalTo(expectedSimpleSolution()));
+	}
+
+	@Test
+	public void calculate() {
+		double input[][] = new double[][] {
+				{ 9.0, 3.0, 2.0,  2.0, 10.0 },
+				{ 3.0, 1.0, 1.0,  2.0, 15.0 },
+				{ 2.0, 1.0, 3.0,  6.0,  9.0 },
+				{ 2.0, 2.0, 6.0,  9.0,  3.0 }
+		};
+
+		Result result = m.calculate(input);
+
+		assertThat(result.getMatrixRegister().get("G"), equalTo(expectedMatrix()));
+		assertThat(result.getSolution(), equalTo(expectedSolution()));
+	}
+
+	private double[] expectedSimpleSolution() {
+		return new double[] { 1.0, 0.0, 1.0 };
+	}
+
+	private double[][] expectedSimpleMatrix() {
+		return new double[][]{
+				{  2.0, 0.0, 0.0 },
+				{  1.0, 3.0, 0.0 },
+				{ -2.0, 2.0, 1.0 }
+		};
+	}
+
+	private double[][] expectedTransposedSimpleMatrix() {
+		return new double[][]{
+				{ 2.0, 1.0, -2.0 },
+				{ 0.0, 3.0,  2.0 },
+				{ 0.0, 0.0,  1.0 }
+		};
 	}
 
 	private double[] expectedSolution() {
